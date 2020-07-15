53703ee9a605318c5c0bdd35b16e2df4a3718464
wb_glb <weibin9966@qq.com>
2020-07-15 15:08:25 +0800

    模块初始化

---
+++ claims-form/src/main/java/com/dm/claims/service/impl/FormServiceImpl.java
@@ -0,0 +1,23 @@
+package com.dm.claims.service.impl;
+
+import com.dm.claims.dao.FormDao;
+import com.dm.claims.entity.FormEntity;
+import com.dm.claims.pojo.PageUtils;
+import com.dm.claims.pojo.Query;
+import com.dm.claims.service.FormService;
+import org.springframework.stereotype.Service;
+import java.util.Map;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+
+
+@Service("formService")
+public class FormServiceImpl extends ServiceImpl<FormDao, FormEntity> implements FormService {
+
+
+    @Override
+    public PageUtils queryPage(Map<String, Object> params) {
+        return null;
+    }
+}
